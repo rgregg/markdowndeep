ea745dc437d715f3079366e3a128ce3e3bd8265c
Ryan Gregg <rgregg@users.noreply.github.com>
2016-03-15 23:25:54 -0700

    Cleanup folders

--- makenpm.bat
+++
@@ -1,7 +0,0 @@
-cd NodePackage
-copy ..\MarkdownDeepJS\MarkdownDeep.js 
-copy ..\MarkdownDeepJS\MarkdownDeep*.js .\clientSide\
-copy ..\MarkdownDeepJS\mdd_*.* .\clientSide\
-call npm version patch -m "Version %s"
-call npm publish
-cd ..
